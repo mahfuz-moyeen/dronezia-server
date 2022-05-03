f79311a1c3430dc9b1c9b27721f75db884dc5a60
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-05-04 00:10:00 +0600

    add mongodb server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 // port
 const port = process.env.PORT || 5000;
@@ -9,6 +11,17 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(cors());
+
+// mongodb server
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irqio.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('mongo db runinng');
+    client.close();
+});
+
 
 
 // server root
