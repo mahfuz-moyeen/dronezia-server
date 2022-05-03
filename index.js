2d17f3dcb522053097b5a7bd8fdd78e34c7a71d4
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-05-04 03:06:10 +0600

    get single inventory item bt id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -19,12 +19,7 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r2ma4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   console.log('work');
-//   // perform actions on the collection object
-//   client.close();
-// });
+
 
 async function run() {
     try {
@@ -35,12 +30,20 @@ async function run() {
         // // Inventory section API//
         // /////////////////////////
 
-        // // get all inventory
+        // get all inventory items
         app.get('/inventory', async (req, res) => {
             const query = {}
             const cursor = inventoriesCollection.find(query)
             const inventories = await cursor.toArray();
             res.send(inventories)
+        })
+
+        // get single inventory item by id
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const inventory = await inventoriesCollection.findOne(query)
+            res.send(inventory);
         })
     }
     finally {
